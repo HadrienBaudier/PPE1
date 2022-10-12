1cc2ad9d5ab6ae9204a0fc397c9b93124aedbbea
Hadrien Baudier <hadrienbaudier@gmail.com>
2022-10-12 10:32:17 +0200

    Voici le premier script

---
+++ premier_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+cd /home/hadrien/Downloads/Fichiers
+echo "où suis-je ?"
+pwd
+#traitement de l'année 2016, on crée on écrase le fichier sortie.txt
+echo "pour l'année 2016" > sortie.txt
+grep "Location" 2016*.ann | wc -l >> sortie.txt
+#traitement de l'année 2017, on écrit à la suite du fichier sortie.txt
+echo "pour l'année 2017" >> sortie.txt
+grep "Location" 2017*.ann | wc -l >> sortie.txt
+# traitement de l'année	2018, on écrit à la suite du fichier sortie.txt
+echo "pour l'année 2018" >> sortie.txt
+grep "Location" 2018*.ann | wc -l >> sortie.txt
